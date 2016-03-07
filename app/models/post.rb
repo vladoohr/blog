b781ac8ee192fce5374d61858002431d77911269
Vladimir Tomanovski <vladoohr@gmail.com>
2016-03-07 22:27:15 +0100

    Add validation to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :description, presence: true, length: {minimum: 3}
+
+	before_save {self.title = title.slice(0,1).capitalize + title.slice(1..-1)}
 end
