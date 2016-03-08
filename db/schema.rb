d53b6866a7198b9eb95fea32116b8959beb9b284
Vladimir Tomanovski <vladoohr@gmail.com>
2016-03-08 21:17:06 +0100

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307210607) do
+ActiveRecord::Schema.define(version: 20160308201626) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160307210607) do
     t.string   "username"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
